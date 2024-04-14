5b9909a989d59ef9f979e9e4632dddde44153c59
Green Sky <green@g-s.xyz>
2024-04-14 12:41:54 +0200

    add serializer (setup) for the basic message components

---
+++ src/solanaceae/message3/nj/message_components_serializer.cpp
@@ -0,0 +1,34 @@
+#include "./message_components_serializer.hpp"
+
+#include "./message_components.hpp"
+
+void registerMessageComponents(MessageSerializerNJ& msnj) {
+	msnj.registerSerializer<Message::Components::Timestamp>();
+	msnj.registerDeserializer<Message::Components::Timestamp>();
+	msnj.registerSerializer<Message::Components::TimestampProcessed>();
+	msnj.registerDeserializer<Message::Components::TimestampProcessed>();
+	msnj.registerSerializer<Message::Components::TimestampWritten>();
+	msnj.registerDeserializer<Message::Components::TimestampWritten>();
+	msnj.registerSerializer<Message::Components::ContactFrom>();
+	msnj.registerDeserializer<Message::Components::ContactFrom>();
+	msnj.registerSerializer<Message::Components::ContactTo>();
+	msnj.registerDeserializer<Message::Components::ContactTo>();
+	msnj.registerSerializer<Message::Components::TagUnread>();
+	msnj.registerDeserializer<Message::Components::TagUnread>();
+	msnj.registerSerializer<Message::Components::Read>();
+	msnj.registerDeserializer<Message::Components::Read>();
+	msnj.registerSerializer<Message::Components::MessageText>();
+	msnj.registerDeserializer<Message::Components::MessageText>();
+	msnj.registerSerializer<Message::Components::TagMessageIsAction>();
+	msnj.registerDeserializer<Message::Components::TagMessageIsAction>();
+
+	// files
+	//_sc.registerSerializerJson<Message::Components::Transfer::FileID>()
+	//_sc.registerSerializerJson<Message::Components::Transfer::FileInfo>();
+	//_sc.registerDeSerializerJson<Message::Components::Transfer::FileInfo>();
+	//_sc.registerSerializerJson<Message::Components::Transfer::FileInfoLocal>();
+	//_sc.registerDeSerializerJson<Message::Components::Transfer::FileInfoLocal>();
+	//_sc.registerSerializerJson<Message::Components::Transfer::TagHaveAll>();
+	//_sc.registerDeSerializerJson<Message::Components::Transfer::TagHaveAll>();
+}
+
---
+++ src/solanaceae/message3/nj/message_components_serializer.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <solanaceae/message3/message_serializer.hpp>
+
+void registerMessageComponents(MessageSerializerNJ& msnj);
+
